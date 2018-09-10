97122ddb59d154cbeb0f099416d2c6e6b822b8ae
Unknown <tofferlinville@gmail.com>
2018-09-10 10:10:52 -0600

    Added Player Code

--- 2D Game/Assets/Scripts/charactermovement.cs
+++ 2D Game/Assets/Scripts/charactermovement.cs
@@ -2,15 +2,46 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class charactermovement : MonoBehaviour {
+public class charactermovement : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Player Movement Variables
+    public int MoveSpeed;
+    public float JumpHeight;
+
+    // Player grounded Variables
+    public Transform GroundCheck;
+    public float GroundCheckRadius;
+    public LayerMask WhatIsGround;
+    private bool grounded;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+
+
+    private void FixedUpdate()
+    {
+        grounded = Physics2D.OverlapCircle(GroundCheck.position, GroundCheckRadius, WhatIsGround);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // This code makes the character jump
+        if(Input.GetKeyDown(KeyCode.Space) && grounded)
+        {
+            Jump();
+        }
+    }
+
+    public void Jump()
+    {
+        GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, JumpHeight);
+    }
+
 }
