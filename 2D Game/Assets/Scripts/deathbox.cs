6a4661a67be14b4131885ab01187bc457c699ef9
Unknown <tofferlinville@gmail.com>
2018-09-20 10:28:03 -0600

    Added Deathbox & Score Basics

---
+++ 2D Game/Assets/Scripts/deathbox.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class deathbox : MonoBehaviour {
+
+
+    void OnTriggerEnter2D(GameObject other){
+
+        if (other.name == "PC")
+        {
+            Debug.Log("PC Enters Death Zone");
+            Destroy(other);
+        }
+     }
+  }
---
+++ 2D Game/Assets/Scripts/scoremanager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngin;
+using UnityEngine.UI;
+
+public class NewBehaviourScript : MonoBehaviour {
+    public static int score;
+
+    Text scoretext;
+
+	// Use this for initialization
+	void Start () {
+        scoretext = GetComponent<Text>();
+
+        score = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (score < 0)
+            score = 0;
+        scoretext.text = " " + score;
+		
+	}
+
+    public static void AddPoints (int pointsToAdd){
+        score += pointsToAdd;
+    }
+
+    public static void Reset () {
+        score = 0;
+    }
+}
