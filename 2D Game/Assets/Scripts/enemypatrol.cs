faa971e3368e60b56e74fa550053f09a4a742bac
Unknown <tofferlinville@gmail.com>
2018-09-26 10:29:03 -0600

    Added Enemy

---
+++ 2D Game/Assets/Scripts/enemypatrol.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemypatrol : MonoBehaviour{
+    // Movement Variables
+    public float Movespeed;
+    public bool Moveright;
+
+    // Wall Check
+    public Transform WallCheck;
+    public float WallCheckRadius;
+    public LayerMask WhatIsWall;
+    private bool HittingWall;
+    // Edge Check
+    private bool NotAtEdge;
+    public Transform EdgeCheck;
+
+    //Update is once per frame
+    void Update(){
+        NotAtEdge = Physics2D.OverlapCircle(EdgeCheck.position, WallCheckRadius, WhatIsWall);
+        HittingWall = Physics2D.OverlapCircle(WallCheck.position, WallCheckRadius, WhatIsWall);
+        if (HittingWall || !NotAtEdge){
+            Moveright = !Moveright;
+        }
+           
+        if (Moveright){
+            transform.localScale = new Vector3(-6f, 6f, 6f);
+            GetComponent<Rigidbody2D>().velocity = new Vector2(Movespeed, GetComponent<Rigidbody2D>().velocity.y);
+        }
+            
+        else{
+            transform.localScale = new Vector3(6f, 6f, 6f);
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-Movespeed, GetComponent<Rigidbody2D>().velocity.y);
+        }
+
+    }
+       
+} 
