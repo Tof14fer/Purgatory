99c0bb8313d6f8821d234e239c785a824c1d4c18
Unknown <tofferlinville@gmail.com>
2018-10-10 10:29:52 -0600

    Added Projectile

---
+++ 2D Game/Assets/Scripts/projectile.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class projectile : MonoBehaviour {
+    public float Speed;
+    public Rigidbody2D PC;
+
+    public GameObject EnemyDeath;
+
+    public GameObject ProjectileParticle;
+
+    public int PointsForKill;
+
+
+	// Use this for initialization
+	void Start () {
+        // PC = FindObjectOfType<Rigidbody2D>;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GetComponent<Rigidbody2D>().velocity = new Vector2(Speed, GetComponent<Rigidbody2D>().velocity.y);
+
+        if (PC.transform.localScale.x < 0)
+            Speed = -Speed;
+	}
+
+	private void OnTriggerEnter2D(Collider2D other){
+        if(other.tag == "Enemy"){
+            Instantiate(EnemyDeath, other.transform.position, other.transform.rotation);
+            Destroy(other.gameObject);
+            scoremanager.AddPoints(PointsForKill);
+        }
+
+        Instantiate(ProjectileParticle, transform.position, transform.rotation);
+        Destroy (gameObject);
+	}
+}
---
+++ 2D Game/Assets/Scripts/shootscript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class shootscript : MonoBehaviour {
+    public Transform FirePoint;
+    public GameObject Projectile;
+
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.RightControl))
+            Instantiate(Projectile, FirePoint.position, FirePoint.rotation);
+	}
+}
