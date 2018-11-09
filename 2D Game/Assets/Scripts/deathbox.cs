8d9474e3d5fd16c636a566448a44766b5bf2e0c2
Unknown <tofferlinville@gmail.com>
2018-11-09 10:27:50 -0700

    Added Enemy Boss

---
+++ 2D Game/Assets/Scripts/deathbox.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class deathbox : MonoBehaviour {
+	private void OnTriggerEnter2D(Collider2D other){
+
+        if(other.name == "PC")
+        {
+            Debug.Log("Player Enters Death Zone");
+            Destroy(other);
+        }
+
+	}
+}
