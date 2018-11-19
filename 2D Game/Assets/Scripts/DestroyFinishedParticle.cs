fd4a70877285f691f914d669299d683d667c7616
Unknown <tofferlinville@gmail.com>
2018-11-19 10:09:59 -0700

    Fixed Particles

---
+++ 2D Game/Assets/Scripts/DestroyFinishedParticle.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyFinishedParticle : MonoBehaviour {
+
+    private ParticleSystem thisParticelSystem;
+
+	// Use this for initialization
+	void Start () {
+        thisParticelSystem = GetComponent<ParticleSystem>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (thisParticelSystem.isPlaying)
+            return;
+
+        Destroy(gameObject);
+	}
+
+	private void OnBecameInvisible(){
+        Destroy(gameObject);
+	}
+
+}
