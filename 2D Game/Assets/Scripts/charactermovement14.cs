1c253d278e885d507d8baf5de6b911a3fe7544a8
Unknown <tofferlinville@gmail.com>
2018-12-10 10:08:41 -0700

    Senic is Back Bois

--- 2D Game/Assets/Scripts/charactermovement14.cs
+++ 2D Game/Assets/Scripts/charactermovement14.cs
@@ -81,10 +81,10 @@ public class charactermovement14 : MonoBehaviour
 
         // Player Flip
         if (GetComponent<Rigidbody2D>().velocity.x > 0)
-            transform.localScale = new Vector3(4f, 4f, 1f);
+            transform.localScale = new Vector3(0.09f, 0.09f, 1f);
         
         else if (GetComponent<Rigidbody2D>().velocity.x < 0)
-            transform.localScale = new Vector3(-4f, 4f, 1f);
+            transform.localScale = new Vector3(-0.09f, 0.09f, 1f);
 
     }
     public void Jump(){
